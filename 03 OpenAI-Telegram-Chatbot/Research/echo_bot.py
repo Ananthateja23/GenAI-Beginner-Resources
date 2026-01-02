4cfb60f9cb8204b54eb8d7cfc9588d98b8e477a8
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-01-02 09:54:14 +0530

    Refactor project structure and rename files

---
+++ 03 OpenAI-Telegram-Chatbot/Research/echo_bot.py
@@ -0,0 +1,38 @@
+import logging
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+# print(TELEGRAM_BOT_TOKEN)
+
+
+# configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token = TELEGRAM_BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands = ["start", "help"])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives message with `/start` or `/help` command
+    """
+    await message.reply("Hi\nI am Echo Bot!\nPowered by aiogram.")
+
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    """
+    This will return echo
+    """
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
